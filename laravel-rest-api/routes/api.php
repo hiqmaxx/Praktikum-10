7430c50464846160ea1869ece1f6b541240c3a61
hiqmaxx <hiqma2008@gmail.com>
2025-04-11 10:04:39 +0700

    Initial Commit

---
+++ laravel-rest-api/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\HelloWorldController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/hello', [HelloWorldController::class, 'index']);
+Route::get('/hello/{id}', [HelloWorldController::class, 'show']);
+Route::post('/hello', [HelloWorldController::class, 'store']);
+Route::put('/hello/{id}', [HelloWorldController::class, 'update']);
+Route::delete('/hello/{id}', [HelloWorldController::class, 'destroy']);
